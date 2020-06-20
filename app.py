e15e122ea43ddaad0f6aef1cd7351bf97c5106df
John McAllister <43422088+jmcall10@users.noreply.github.com>
2020-06-20 16:05:35 -0400

    Create "all_things" array
    Passes issues and coordinates in one array, as opposed to passing two different arrays

--- app.py
+++ app.py
@@ -2,36 +2,37 @@ from flask import Flask, render_template
 import tweepy
 import googlemaps
 
+
 app = Flask(__name__)
 
-#home page
 @app.route('/')
 def home():
-	#removed keys for privacy reason
-    gmaps_key = googlemaps.Client(key = "google_maps_api_key")
-    ACCESS_TOKEN = 'twitter_access_token'
-    ACCESS_SECRET = 'twitter_access_secret'
-    CONSUMER_KEY = 'consumer_key'
-    CONSUMER_SECRET = 'consumer_secret_key'
+    gmaps_key = googlemaps.Client(key = [removed])
+    ACCESS_TOKEN = '[removed]'
+    ACCESS_SECRET = '[removed]'
+    CONSUMER_KEY = '[removed]'
+    CONSUMER_SECRET = '[removed]'
 
     # Setup access to API
+
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     api = tweepy.API(auth)
-	
-	#pull tweets from Twitter
+
+
+    a = 0
     traffic_tweets = api.user_timeline('QCtrafficalerts')
 
     #Making blank arrays, eventually for flask use
     lat_cords = []
     lng_cords = []
     all_cords = []
+    all_times = []
+    all_issues = []
 
     for tweet in traffic_tweets:
-		#Remove "Reported:" from text
         tweet.text = tweet.text[10:]
-		
-        #Cleaning up tweet formatting
+        all_times.append(tweet.created_at)
         l = str(tweet.text)
         m = l.replace(" LN ", " LANE ")
         n = m.replace(" &amp; ", " & ")
@@ -47,24 +48,19 @@ def home():
         x = w.replace(" ST ", " STREET ")
         y = x.replace(" CT ", " COURT ")
         z = y.split("at", -1)
-		
-		#Seperate the issue (signal outage, accident, dangerous driving) and the location (address or intersection)
+
         location = z[1].split("#", -1)
         issue = z[0]
-
-        print(issue)
+        all_issues.append(issue)
+        #print(issue)
         specific = str(location[0].rstrip())
 
         location = str("  " + specific +  ", CHARLOTTE, NC")
-        print(location)
-		
-		#Left this comment here for 'created at' incase I add functionality for this later
+        #print(location)
+
         #print("   " + str(tweet.created_at))
-		
-		#geocode the result -> returns lat,long cords if given address. Google Maps Geocoding API.
         geocode_result = gmaps_key.geocode(location)
-		
-		#Add cords to array
+
         try:
             lat = str(geocode_result[0]["geometry"]["location"]["lat"])
             lat_cords.append(lat)
@@ -76,16 +72,11 @@ def home():
             lat = None
             lng = None
         print()
-		
-	#Combine both sets of cords into one array	
     for x in range(len(lat_cords)):
         a = lat_cords[x]
         b = lng_cords[x]
         all_cords.append(str(a) + ", " + str(b))
-    print(all_cords)
-	#Passes all_cords array to html template
-    return render_template("MyMap.html", content = all_cords)
+    all_things = list(zip(all_issues,all_cords))
+    #print(all_things)
 
-
-
-
+    return render_template("MyMap.html", all = all_things)
